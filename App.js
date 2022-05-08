24ce6e039a9afd1404a2938d48577ffcc2d1798d
SGeri <sarffygeri@gmail.com>
2022-05-08 16:21:55 +0200

    test search field

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View, Text, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 const DEFAULT_REGION = {
@@ -12,7 +20,6 @@ const DEFAULT_REGION = {
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Teszt</Text>
       <MapView style={styles.map} region={DEFAULT_REGION}>
         <Marker
           title="Huroc Party Offline #1"
@@ -29,17 +36,35 @@ export default function App() {
           }}
         />
       </MapView>
+
+      <View
+        style={{
+          position: "absolute",
+          width: "100%",
+          top: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+          padding: 20,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            height: 50,
+            justifyContent: "center",
+            borderWidth: 2,
+            borderRadius: 30,
+            borderColor: "purple",
+            backgroundColor: "white",
+            padding: 10,
+          }}
+          activeOpacity={0.6}
+        >
+          <Text>Rendezvény keresése...</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
